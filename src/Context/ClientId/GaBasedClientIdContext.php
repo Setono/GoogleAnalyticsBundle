0f80528516ff2f4d881d5061def3ce1e028d36ea
Joachim Løvgaard <joachim@loevgaard.dk>
2022-12-06 13:41:33 +0100

    Added todo

--- src/Context/ClientId/GaBasedClientIdContext.php
+++ src/Context/ClientId/GaBasedClientIdContext.php
@@ -14,6 +14,7 @@ final class GaBasedClientIdContext implements ClientIdContextInterface
 
     public function getClientId(): string
     {
+        // todo is it really only the client id part that is the client id? Is it not the client id + timestamp together?
         return (string) $this->gaContext->getGa()->clientId;
     }
 }
