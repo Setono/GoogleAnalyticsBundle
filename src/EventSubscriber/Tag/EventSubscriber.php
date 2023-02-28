c8780cd99555fb331961dd1608a1a7620278384c
Joachim Løvgaard <joachim@loevgaard.dk>
2023-02-28 14:13:29 +0100

    Add subscriber that injects 'gtags'

--- src/EventSubscriber/Tag/EventSubscriber.php
+++ src/EventSubscriber/Tag/EventSubscriber.php
@@ -4,42 +4,71 @@ declare(strict_types=1);
 
 namespace Setono\GoogleAnalyticsBundle\EventSubscriber\Tag;
 
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use Setono\Consent\ConsentCheckerInterface;
 use Setono\Consent\Consents;
-use Setono\GoogleAnalyticsBundle\Provider\PropertyProviderInterface;
-use Setono\TagBag\Tag\ConsentableScriptTag;
+use Setono\GoogleAnalyticsBundle\Stack\EventStackInterface;
+use Setono\GoogleAnalyticsMeasurementProtocol\Request\Body\Event\Event;
+use Setono\TagBag\Tag\ConsentableInlineScriptTag;
 use Setono\TagBag\Tag\InlineScriptTag;
-use Setono\TagBag\Tag\ScriptTag;
 use Setono\TagBag\Tag\TagInterface;
 use Setono\TagBag\TagBagInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
-final class EventSubscriber implements EventSubscriberInterface
+final class EventSubscriber implements EventSubscriberInterface, LoggerAwareInterface
 {
+    private LoggerInterface $logger;
+
     public function __construct(
         private readonly TagBagInterface $tagBag,
+        private readonly EventStackInterface $eventStack,
+        private readonly ?ConsentCheckerInterface $consentChecker,
         private readonly bool $consentEnabled,
     ) {
+        $this->logger = new NullLogger();
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
-            KernelEvents::RESPONSE => ['add', -1000],
+            KernelEvents::RESPONSE => ['add', -800],
         ];
     }
 
-    public function add(RequestEvent $event): void
+    public function add(ResponseEvent $responseEvent): void
     {
-        if (!$event->isMainRequest()) {
+        if (!$responseEvent->isMainRequest() || $this->eventStack->isEmpty()) {
             return;
         }
 
-        if ($this->consentEnabled) {
-            $this->tagBag->add(ConsentableScriptTag::create($src, Consents::CONSENT_STATISTICS));
-        } else {
-            $this->tagBag->add(ScriptTag::create($src)->defer()->withSection(TagInterface::SECTION_HEAD));
+        foreach ($this->eventStack->popClientSide() as $event) {
+            if ($this->consentEnabled && null !== $this->consentChecker && !$this->consentChecker->isGranted(Consents::CONSENT_STATISTICS)) {
+                $this->tagBag->add(ConsentableInlineScriptTag::create($this->generateGtag($event), Consents::CONSENT_STATISTICS));
+            } else {
+                $this->tagBag->add(InlineScriptTag::create($this->generateGtag($event))->withSection(TagInterface::SECTION_HEAD));
+            }
         }
+    }
+
+    private function generateGtag(Event $event): string
+    {
+        try {
+            $json = json_encode($event->jsonSerialize()['params'], \JSON_THROW_ON_ERROR | \JSON_FORCE_OBJECT);
+        } catch (\JsonException $e) {
+            $this->logger->error(sprintf('Could not json encode the event %s. The JSON error was: %s', $event->getEventName(), $e->getMessage()));
+
+            return sprintf("console.error('Could not json encode the event %s');", $event->getEventName());
+        }
+
+        return sprintf("gtag('event', '%s', %s);", $event->getEventName(), $json);
+    }
+
+    public function setLogger(LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
     }
 }
--- src/EventSubscriber/Tag/LibrarySubscriber.php
+++ src/EventSubscriber/Tag/LibrarySubscriber.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Setono\GoogleAnalyticsBundle\EventSubscriber\Tag;
 
+use Setono\Consent\ConsentCheckerInterface;
 use Setono\Consent\Consents;
 use Setono\GoogleAnalyticsBundle\Provider\PropertyProviderInterface;
 use Setono\TagBag\Tag\ConsentableScriptTag;
@@ -20,6 +21,7 @@ final class LibrarySubscriber implements EventSubscriberInterface
     public function __construct(
         private readonly TagBagInterface $tagBag,
         private readonly PropertyProviderInterface $propertyProvider,
+        private readonly ?ConsentCheckerInterface $consentChecker,
         private readonly bool $consentEnabled,
     ) {
     }
@@ -41,20 +43,20 @@ final class LibrarySubscriber implements EventSubscriberInterface
         // when adding tags in other subscribers, since they will be using the gtag method, and therefore we don't
         // have to check if there are any properties when adding all other tags
         $this->tagBag->add(InlineScriptTag::create('function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date);')
-            ->withSection(TagInterface::SECTION_HEAD));
+            ->withSection(TagInterface::SECTION_HEAD)->withPriority(100));
 
         $properties = $this->propertyProvider->getProperties();
         if (count($properties) > 0) {
             $this->tagBag->add(InlineScriptTag::create(sprintf('gtag("config", "%s");', $properties[0]->measurementId))
-                ->withSection(TagInterface::SECTION_HEAD))
+                ->withSection(TagInterface::SECTION_HEAD)->withPriority(90))
             ;
 
             $src = sprintf('https://www.googletagmanager.com/gtag/js?id=%s', $properties[0]->measurementId);
 
-            if ($this->consentEnabled) {
+            if ($this->consentEnabled && null !== $this->consentChecker && !$this->consentChecker->isGranted(Consents::CONSENT_STATISTICS)) {
                 $this->tagBag->add(ConsentableScriptTag::create($src, Consents::CONSENT_STATISTICS));
             } else {
-                $this->tagBag->add(ScriptTag::create($src)->defer()->withSection(TagInterface::SECTION_HEAD));
+                $this->tagBag->add(ScriptTag::create($src)->defer()->withSection(TagInterface::SECTION_HEAD)->withPriority(80));
             }
         } else {
             $this->tagBag->add(InlineScriptTag::create('console.error("[Setono Google Analytics Bundle] You have not configured any Google Analytics properties.")'));
